b4c025d43f22733f4dd94d7c4a216964cb0db72f
jorden <378015802@qq.com>
2016-10-15 12:00:50 +0800

    commit page1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
  
-  <!-- Initialize Swiper -->
-   
-    window.onload=function(){
-    	 var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-    }
-   
-        
-
-
-    });
-   
+  
     
